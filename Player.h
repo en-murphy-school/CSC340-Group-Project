3f8d621a4a0ff3ef00579d22dce8ac205fe5b5d3
judoak <aydankaneshiro02@gmail.com>
2024-05-10 17:02:41 -0700

    Player.h & Player.cpp

--- Player.h
+++ Player.h
@@ -1,17 +1,30 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include "Items.h"
+#include <string>
 
 
 class Player {
 private:
     int money;
     std::string name;
-    Item[] itemList;
+    Item[]* itemList;
     Node* currentLocation;
 
 public:
-   // Constructors and getters/setters
+// Constructors and getters/setters
+    Player();
+    Player(int newMoney, std::string newName, Item[]* newItemList, Node* newCurrentLocation);
 
+    std::string GetName();
+    int GetMoney();
+    Item[] GetItemList();
+    Node* GetCurrentLocation();
+
+    void SetName(std::string newName);
+    void SetMoney(int newMoney);
+    void SetItemList(Item[]* newItemList);
+    void SetCurrentLocation(Node* newCurrentLocation);
 };
 
 #endif
