ad6505feb27180bde1ea17f5fac5d61cf60db738
BuildTools <unconfigured@null.spigotmc.org>
2024-05-10 18:36:15 -0700

    changed main

--- main.cpp
+++ main.cpp
@@ -1,40 +1,90 @@
-//main
-
 #include <iostream>
-#include <fstream> 
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "items/Weapon.h"
+#include "items/Armor.h"
 
 using namespace std;
 
-void generateWorld(){
-// Read from a file input
-// First make new node with the name of the location
-// Set the values OF this new node with file contents
-// Add it to the linkedList
+Item* items[100];
+int itemCount = 0;
 
-//repeat the process (read the next line)
+string formatName(const string& inputName) {
+    string formattedName;
+    for (char c : inputName) 
+    {
+        if (isupper(c)) 
+        {
+            formattedName += ' '; 
+        }
+        formattedName += c;
+    }
+    return formattedName;
+}
 
-// Files will be formatted kinda like this: (find a way to split data, whether a special symbol etc.)
-
-/*
-Ganburg:: Welcome to the town of Hamburg, a magic marvel... :: SingleItem-Shield(5,4  #stats) :: storeItems-Sword(3,5)-potion(5,0) 
-Homesrong :: ....
-
-*/
-
+string formatDescription(const string& inputDescription) {
+    string formattedDescription;
+    for (char c : inputDescription) {
+        if (c == '-') {
+            formattedDescription += ' '; // Replace dash with space
+        } else {
+            formattedDescription += c;
+        }
+    }
+    return formattedDescription;
 }
 
 
-int getUserChoice(){
-    //make a menu to get the user option, 
+void generateItems() {
+    ifstream inputFile("items.txt"); 
+
+    if (!inputFile) {
+        cerr << "Failed to open file." << endl;
+        return; 
+    }
+
+
+    string type;
+    string name;
+    string description;
+    int cost;
+    bool available;
+    int value1, value2;
+
+    while (inputFile >> type >> name >> description >> cost >> available >> value1 >> value2) {
+        if (type == "WEAPON") {
+            name = formatName(name);
+            description = formatDescription(description);
+            Weapon* newWeapon = new Weapon(name, description, cost, available, value1, value2);
+            cout << "Weapon Made" << endl;
+            items[itemCount] = newWeapon;
+            itemCount++;
+
+        } else if (type == "ARMOR") {
+            name = formatName(name);
+            description = formatDescription(description);
+            Armor* newArmor = new Armor(name, description, cost, available, value1, value2);
+            cout << "Armor Made" << endl;
+            items[itemCount] = newArmor;
+            itemCount++;
+
+        }
+        
+    }
+
+    inputFile.close(); 
 }
 
 
-int main(){
 
-    generateWorld();
+int main() {
 
-    // Call a while loop to keep asking user for prompt (until town is left etc.)
-    //switch case after calling getUserChoice
+    generateItems();
+
+    
+
 
     return 0;
 }
