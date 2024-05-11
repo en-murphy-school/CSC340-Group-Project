66b95842fca96359f6b6770a4124829dfdfbe66e
AndyJ21 <huehuecho2121@gmail.com>
2024-05-10 18:50:58 -0700

    Location class done

--- Location/Location.cpp
+++ Location/Location.cpp
@@ -1,5 +1,75 @@
-//
-// Created by Christian Rivera on 5/9/24.
-//
-
 #include "Location.h"
+
+Location::Location() {
+    (*this).name = "Defaul Name";
+    (*this).description = "Default Description";
+}
+
+Location::Location(const string& name, const string& description, vector<Item*>& storeItems) {
+    (*this).name = name;
+    (*this).description = description;
+    (*this).storeItems = storeItems;
+}
+
+Location::Location(const string& name, const string& description, vector<Item*>& storeItems, 
+                    Item* hiddenItem) {
+    (*this).name = name;
+    (*this).description = description;
+    (*this).storeItems = storeItems;
+    (*this).hiddenItem = hiddenItem;
+}
+
+string Location::getLocationName() const {
+    return (*this).name;
+}
+
+void Location::setLocationName(const string& name) {
+    (*this).name = name;
+}
+
+string Location::getLocationDescription() const {
+    return (*this).description;
+}
+
+void Location::setLocationDescription(const string& description) {
+    (*this).description = description;
+}
+
+vector<Item*> Location::getStoreItems() const {
+    if ((*this).storeItems.size() == 0) {
+        cout << "There are no items in this store!" << endl;
+    }
+
+    return (*this).storeItems;
+}
+
+void Location::addStoreItem(Item* item) {
+    (*this).storeItems.push_back(item);
+}
+
+bool Location::findItem(const string& itemName) const {
+    for (vector<Item*>::const_iterator it = (*this).storeItems.begin(); it != (*this).storeItems.end(); it++) {
+        if ((*it)->getItemName() == itemName) {
+            return true; 
+        }
+    }
+    return false;
+}
+
+Item* Location::getHiddentItem() const {
+    return (*this).hiddenItem;
+}
+
+void Location::setHiddenItem(Item* item) {
+    (*this).hiddenItem = item;
+}
+
+void Location::printStoreItems() {
+    for (int i = 0; i < storeItems.size(); i++) {
+        storeItems.at(i)->printItem();
+        cout << endl;
+    }
+}
+
+
+
--- Location/Location.h
+++ Location/Location.h
@@ -1,14 +1,32 @@
-//
-// Created by Christian Rivera on 5/9/24.
-//
+#ifndef LOCATION_H
+#define LOCATION_H
 
-#ifndef CSC340_GROUP_PROJECT_LOCATION_H
-#define CSC340_GROUP_PROJECT_LOCATION_H
-
-
+#include <string>
+#include <vector>
+#include "Item.h"
+#include "Armor.h"
+#include "Weapon.h"
 class Location {
-
+    private:
+        string name;
+        string description;
+        vector<Item*> storeItems;
+        Item* hiddenItem;
+    public:
+        Location();
+        Location(const string& name, const string& description, vector<Item*>& storeItems);
+        Location(const string& name, const string& description, vector<Item*>& storeItems, Item* hiddenItem);
+        string getLocationName() const;
+        void setLocationName(const string& name);
+        string getLocationDescription() const;
+        void setLocationDescription(const string& description);
+        vector<Item*> getStoreItems() const;
+        void addStoreItem(Item* item);
+        bool findItem(const string& itemName) const;
+        Item* getHiddentItem() const;
+        void setHiddenItem(Item* item);
+        void printStoreItems();
 };
 
 
-#endif //CSC340_GROUP_PROJECT_LOCATION_H
+#endif 
